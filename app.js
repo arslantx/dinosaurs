bb3f705f1648bc48a4c2059724ca0fc0b4a89a74
Arslan Haytiyev <arslanh@gmail.com>
2020-12-13 22:11:51 -0600

    Adding form data to human

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
+// Common parent constructor
+function Creature(name, weight, height, diet) {
+    this.name = name;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+}
 
-    // Create Dino Constructor
+// Create Dino Constructor
+function Dino(dino) {
+    Creature.call(this, dino.species, dino.weight, dino.height, dino.diet);
+    this.where = dino.where;
+    this.when = dino.when;
+    this.fact = dino.fact;
+}
 
+// Create Dino Objects
+const dinoArray = dinoDataSource.map(dino => {
+    return new Dino(dino);
+});
 
-    // Create Dino Objects
+// Create Human Object
+const human = new Creature();
 
-
-    // Create Human Object
-
-    // Use IIFE to get human data from form
+// Use IIFE to get human data from form
+function fillHumanData() {
+    (function(humanObj){
+        humanObj.name = document.getElementById('name').value;
+        humanObj.weight = Number(document.getElementById('weight').value);
+        humanObj.diet = document.getElementById('diet').value;
+        const feet = Number(document.getElementById('feet').value);
+        const inches = Number(document.getElementById('inches').value);
+        humanObj.height = feet * 12 + inches;
+    }(human));
+}
 
 
     // Create Dino Compare Method 1
@@ -26,7 +51,14 @@
   
         // Add tiles to DOM
 
-    // Remove form from screen
-
+// Remove form from screen
+function hideForm() {
+    document.getElementById('dino-compare').style.display = 'none';
+}
 
 // On button click, prepare and display infographic
+document.getElementById('btn').addEventListener('click', () => {
+    hideForm();
+    fillHumanData();
+    console.log(human);
+});
